06040dd6ba82b56c00797f2e87a26ce21650db86
Helene Bloch <ln-45@live.fr>
2018-12-20 14:48:36 +0100

    event

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,8 +6,12 @@ use App\Entity\Contact;
 use App\Entity\Event;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EventType extends AbstractType
 {
