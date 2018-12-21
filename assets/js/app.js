85d7cc99bc7e3767333e088349875b6eb9230fa6
Thierry STOLLSTEINER <stollsteiner.thierry@hotmail.fr>
2018-12-21 10:20:20 +0100

    allez ça sonne!

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
 require('@fortawesome/fontawesome-free/css/all.min.css');
-
+const $=require('jquery');
 
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
