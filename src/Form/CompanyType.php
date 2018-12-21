6819374f8159ededb5e6cccb0484548e2d82d18b
Alicia Pilar <alicia.pilar83@gmail.com>
2018-12-21 02:44:33 +0100

    company fix

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -13,22 +13,36 @@ class CompanyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $choices=[
+            'Resident Lab\'O' => 'resident',
+            'Entreprise externe' => 'externe',
+        ];
+        $bool=['Oui'=>1,'Non'=>0];
         $builder
             ->add('name', TextType::class, [
                 'attr' => array('type' => 'text', 'class' => 'color-input size-input'),
                 'label' => 'Nom',
             ])
             ->add('type', ChoiceType::class, [
-                'choices' => [
-                    'Resident Lab\'O' => 'resident',
-                    'Entreprise externe' => 'externe',
-                    ],
-                'expanded' => 'true',
+                'choices' => $choices,
+                'choice_attr' => function($choices, $key, $value) {
+//                     adds a class like attending_yes, attending_no, etc
+                    return ['class' => 'ml-3'];
+                },
+                'expanded' => true,
+                'multiple' => false,
+                'data' => 'name',
+//                'expanded' => 'true',
 //                'attr' => array('class' => 'custom-control-input')
 //                'attr'=> array('type'=> 'radio', 'class' => 'custom-control-input'),
 //                'label' => 'Type'
             ])
-            ->add('partenaire', TextType::class, [
+            ->add('partenaire', ChoiceType::class, [
+                'choices' => $bool,
+                'choice_attr' => function($choices, $key, $value) {
+//                     adds a class like attending_yes, attending_no, etc
+                    return ['class' => 'ml-3'];
+                },
                 'attr' => array('type' => 'text', 'class' => 'color-input size-input'),
                 'label' => 'Partenaire',
             ])
